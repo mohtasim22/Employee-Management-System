c29c49e3d9e99e6f9e7607dbe2b2344f9e5b1cdf
mohtasim22 <62848240+mohtasim22@users.noreply.github.com>
2020-12-14 02:38:16 +0600

    Add files via upload

---
+++ src/MVC_Structure/Model/conn.java
@@ -0,0 +1,20 @@
+package MVC_Structure.Model;
+
+import java.sql.*;
+
+public class conn{
+    
+    public Connection c;
+    public Statement s;
+ 
+    public conn(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///emp","root","12345");
+            s = c.createStatement();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+ 
